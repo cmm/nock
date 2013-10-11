97b24127eeb1af769e386725136f51343a8d15bd
Michael Livshin <dev@cmm.kakpryg.net>
2013-10-11 12:13:36 +0300

    More symbolic op cosmetics
    Signed-off-by: Michael Livshin <dev@cmm.kakpryg.net>

--- symbolic-ops.lisp
+++ symbolic-ops.lisp
@@ -3,37 +3,42 @@
 (in-package nock)
 (in-readtable spel)
 
-(defmacro define-code (name magic-number &rest args)
+(defmacro define-primitive (name magic-number &rest args)
   `(defun ,name (,@args)
      [,magic-number ,@args]))
 
-(define-code %hint 10 hint f)
+(define-primitive %hint 10 hint f)
 
-(define-code %cellp 3 f)
-(define-code %inc 4 f)
-(define-code %eq 5 f)
+(define-primitive %cellp 3 f)
+(define-primitive %inc 4 f)
+(define-primitive %eq 5 f)
 
-(define-code %compose 7 f g)
-(define-code %cell-compose 8 f g)
-(define-code %core-apply 9 method-elt-idx core-maker)
+(define-primitive %compose 7 f g)
+(define-primitive %cell-compose 8 f g)
+(define-primitive %core-apply 9 method-elt-idx core-maker)
 
 (defun %composify (f)
   (%compose (%id) f))
 
-(define-code %elt 0 idx)
+(define-primitive %elt 0 idx)
 
-(define-code %K 1 value)
-(define-code %S 2 a b)
+(define-primitive %K 1 value)
+(define-primitive %S 2 a b)
 (defun %I () (load-time-value (%elt 1) t))
 
-(define-code %if 6 condition then else)
+(define-primitive %if 6 condition then else)
 
-(defun %dec ()
+(defun %dec (f)
+  "The decrementer example from the crash course.
+Except it takes a formula on the right, for symmetry with the
+primitives."
   ;; is that more or less readable than the numeric form?  dunno.
-  (%cell-compose
-   (%K 0)
+  (%compose
+   f
    (%cell-compose
-    (%K (%if (%eq [(%elt 7) (%inc (%elt 6))])
-             (%elt 6)
-             (%core-apply 2 [(%elt 2) (%inc (%elt 6)) (%elt 7)])))
-    (%core-apply 2 (%I)))))
+    (%K 0)
+    (%cell-compose
+     (%K (%if (%eq [(%elt 7) (%inc (%elt 6))])
+              (%elt 6)
+              (%core-apply 2 [(%elt 2) (%inc (%elt 6)) (%elt 7)])))
+     (%core-apply 2 (%I))))))
