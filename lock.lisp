e282bb8416efc65c7e26024262af783315bd56ae
Michael Livshin <dev@cmm.kakpryg.net>
2013-10-14 09:30:32 +0300

    More wankery
    Signed-off-by: Michael Livshin <dev@cmm.kakpryg.net>

--- lock.lisp
+++ lock.lisp
@@ -5,35 +5,59 @@
   '(or cons (integer 0)))
 
 (deftype notom ()
+  "NOck aTOM: an unsigned integer"
   '(integer 0))
 
 (deftype nondex ()
+  "NOck iIDEX: a value suitable for 0"
   '(integer 1))
 
+(deftype formula ()
+  "Nock formula, or Hoon gate: gets a noun, returns a noun.
+NIL is identity because checking for it it cheaper than funcalling
+#'IDENTITY, go figure."
+  '(or (function (noun) (values noun)) null))
+
 (deftype noolean ()
+  "Nock bOOLEAN"
   '(member 0 1))
 
-(deftype formula ()
-  '(or (function (noun) *) null))
-
-(declaim (inline boolify))
-(defun boolify (value)
+(declaim (inline noolify))
+(defun noolify (value)
   (if value 0 1))
 
-(defmacro gen-accessors ()
-  (labels ((ads (i)
+;;; We use the native C*R functions instead of the generic TREE-ELT
+;;; when we can, because those tend to be implemented using clever
+;;; tricks which are unavailable to mere users.  The only wrinkle is
+;;; that they are specified to return NIL for absent elements, so we
+;;; need to wrap each of them in a valid formula lest we lie to the
+;;; compiler.
+(defun wrap-shallow-getter (c*r)
+  (funcall (compile nil
+                    (let ((name (intern (format nil "NOUN-~a" c*r) 'nock)))
+                      `(lambda ()
+                         (declare (optimize (debug 0) (safety 0) (speed 3)))
+                         (named-lambda ,name (.noun.)
+                           (declare (type noun .noun.))
+                           (or (,c*r .noun.)
+                               (nope .noun.
+                                     ,(format nil "too shallow for ~a" c*r)))))))))
+
+(defparameter +shallow-accessors+
+  (labels ((ADs (i)
              (case i
                (2	'(#\A))
                (3	'(#\D))
                (otherwise (multiple-value-bind (quotent remainder)
                               (floor i 2)
-                            (append (ads (+ 2 remainder)) (ads quotent)))))))
-    `#(nil ,@(loop :for i :from 2 :below 32
-                   :collect (find-symbol
-                             (format nil "C~{~a~}R" (ads i))
-                             :cl)))))
-
-(defvar +primitive-accessors+ (gen-accessors))
+                            (append (ADs (+ 2 remainder)) (ADs quotent)))))))
+    (coerce (cons nil (loop :for i :from 2 :below 32
+                            :collect (let ((symbol (find-symbol
+                                                    (format nil "C~{~a~}R" (ADs i))
+                                                    :cl)))
+                                       (cons (shallow-getter symbol)
+                                             (fdefinition `(setf ,symbol))))))
+            'simple-vector)))
 
 (defun tree-elt (idx tree)
   (declare (optimize (debug 0) (safety 1) (speed 3))
@@ -67,34 +91,35 @@
                   ,setter)
                 .formula.)))))
 
+;;; Wrap FUNCALL to accomodate NIL (which stands in for #'IDENTITY)
 (declaim (inline call))
 (defun call (function a)
-  (if (null function)
-      a
-      (funcall function a)))
+  (if function
+      (funcall function a)
+      a))
 
 (defun lock (term)
+  "The lambda-chaining Nock evaluator."
   (let ((noun (nerm-noun term)))
     (ecase (nerm-op term)
       (*	(call (lockf-formula (cdr noun)) (car noun)))
 
-      (?	(boolify (consp noun)))
+      (?	(noolify (consp noun)))
       (+	(1+ noun))
-      (=	(boolify (equal (car noun) (cdr noun))))
+      (=	(noolify (equal (car noun) (cdr noun))))
       (/	(tree-elt (car noun) (cdr noun))))))
 
 (defmacro wrap (&body body &environment env)
   `(locally
-       (declare (optimize (debug 0) (safety 1) (speed 3)))
-     (named-lambda ,(intern (format nil "rule-~{~a~^:~}"
-                                    (reverse (macroexpand-1 '(annotation) env)))
-                            'nock)
+       (declare (optimize (debug 0) (safety 0) (speed 3)))
+     (named-lambda ,(let ((annotation (macroexpand-1 '(annotation) env)))
+                      (intern (format nil "RULE-~{~a~^:~}" (reverse annotation))
+                              'nock))
          (a)
        (declare (type noun a))
        ,@body)))
 
-(declaim (ftype (function (cons) (or null (function (noun) *))) lock-formula))
-
+(declaim (ftype (function (cons) (values formula)) lock-formula))
 (defun lock-formula (noun)
   (declare (optimize (debug 2) (safety 2) (speed 3))
            (type cons noun))
@@ -109,60 +134,68 @@
                                           (cond
                                             ((= 1 b)
                                              nil)
-                                            ((> b (length +primitive-accessors+))
+                                            ((> b (length +shallow-accessors+))
                                              (wrap
                                                (tree-elt b a)))
                                             (t
-                                             (fdefinition (elt +primitive-accessors+ (1- b)))))))
+                                             (car (elt +shallow-accessors+ (1- b)))))))
 
     ([1 b]			$ 22	(constantly b))
+
     ([2 (place b) (place c)]	$ 23	(wrap
                                           (call (lock-formula (call (lockf-formula c) a))
                                                 (call (lockf-formula b) a))))
+
     ([3 (place b)]		$ 24	(wrap
-                                          (boolify (consp (call (lockf-formula b) a)))))
+                                          (noolify (consp (call (lockf-formula b) a)))))
     ([4 (place b)]		$ 25	(wrap
                                           (1+ (the notom (call (lockf-formula b) a)))))
     ([5 (place b)]		$ 26	(wrap
                                           (let ((noun (call (lockf-formula b) a)))
-                                            (boolify (equal (car noun) (cdr noun))))))
+                                            (noolify (equal (car noun) (cdr noun))))))
     ([6 (place b)
         (place c)
         (place d)]		$ 28	(wrap
                                           (if (zerop (the noolean (call (lockf-formula b) a)))
                                               (call (lockf-formula c) a)
                                               (call (lockf-formula d) a))))
+
     ([7 (place b) (place c)]	$ 29	(wrap
                                           (call (lockf-formula c)
                                                 (call (lockf-formula b) a))))
+
     ([8 (place b) (place c)]	$ 30	(wrap
                                           (call (lockf-formula c)
                                                 (let ((product (call (lockf-formula b) a)))
                                                   [product a]))))
-    ([9 b (place c)]		$ 31	(if (> b (length +primitive-accessors+))
+
+    ([9 b (place c)]		$ 31	(if (> b (length +shallow-accessors+))
+
                                             (wrap
                                               (funcall (lambda (core)
                                                          (call (lockf-formula (tree-elt b core))
                                                                core))
                                                        (call (lockf-formula c) a)))
-                                            (let* ((symbol (elt +primitive-accessors+ (1- b)))
-                                                   (getter (fdefinition symbol))
-                                                   (setter (fdefinition `(setf ,symbol))))
-                                              (assert symbol)
+
+                                            (let* ((accessors (elt +shallow-accessors+ (1- b)))
+                                                   (getter (the formula (car accessors)))
+                                                   (setter (cdr accessors)))
                                               (wrap
                                                 (funcall (lambda (core)
                                                            (call (the formula
-                                                                      (let ((formula (funcall getter core)))
+                                                                      (let ((formula (call getter core)))
                                                                         (if (consp formula)
-                                                                            (funcall setter
-                                                                                     (lock-formula formula) core)
+                                                                            (funcall setter (lock-formula formula)
+                                                                                     core)
                                                                             formula)))
                                                                  core))
                                                          (call (lockf-formula c) a))))))
+
     ([10 [_ (place c)]
          (place d)]		$ 32	(wrap
                                           (call (lockf-formula c) a)
                                           (call (lockf-formula d) a)))
+
     ;; TODO jets
     ([10 _ (place c)]		$ 33	(wrap
                                           (call (lockf-formula c) a)))))
